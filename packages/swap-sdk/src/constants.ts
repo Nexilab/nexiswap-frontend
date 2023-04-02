7b580536ab17d35df26d3731fb105d0869754047
git <eae966@gmail.com>
2023-04-02 21:12:07 +0430

    Some Updates in adding nexi chain.

--- packages/swap-sdk/src/constants.ts
+++ packages/swap-sdk/src/constants.ts
@@ -1,5 +1,6 @@
 import { Percent } from '@pancakeswap/swap-sdk-core'
 import { ERC20Token } from './entities/token'
+import { FACTORY_ADDRESS_NEXI } from './constants';
 
 export enum ChainId {
   ETHEREUM = 1,
@@ -12,23 +13,29 @@ export enum ChainId {
 export const ZERO_PERCENT = new Percent('0')
 export const ONE_HUNDRED_PERCENT = new Percent('1')
 
-export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const FACTORY_ADDRESS_BSC = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+
+export const FACTORY_ADDRESS_NEXI = '0x62CB82f53B1688199E023C015ecb8BE76574e8fC'
 
 const FACTORY_ADDRESS_ETH = '0x1097053Fd2ea711dad45caCcc45EfF7548fCB362'
 
 export const FACTORY_ADDRESS_MAP: Record<number, string> = {
   [ChainId.ETHEREUM]: FACTORY_ADDRESS_ETH,
   [ChainId.GOERLI]: FACTORY_ADDRESS_ETH,
-  [ChainId.BSC]: FACTORY_ADDRESS,
+  [ChainId.BSC]: FACTORY_ADDRESS_BSC,
+  [ChainId.NEXI]: FACTORY_ADDRESS_NEXI,
   [ChainId.BSC_TESTNET]: '0x6725f303b657a9451d8ba641348b6761a6cc7a17',
 }
-export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
+export const INIT_CODE_HASH_BSC = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
+
+export const INIT_CODE_HASH_NEXI = '0xf9b463dc99e1db3c33b43f15bebee3c5eb7695df1028d607130a806e80463d9c'
 
 const INIT_CODE_HASH_ETH = '0x57224589c67f3f30a6b0d7a1b54cf3153ab84563bc609ef41dfb34f8b2974d2d'
 export const INIT_CODE_HASH_MAP: Record<number, string> = {
   [ChainId.ETHEREUM]: INIT_CODE_HASH_ETH,
   [ChainId.GOERLI]: INIT_CODE_HASH_ETH,
-  [ChainId.BSC]: INIT_CODE_HASH,
+  [ChainId.BSC]: INIT_CODE_HASH_BSC,
+  [ChainId.NEXI]: INIT_CODE_HASH_NEXI,
   [ChainId.BSC_TESTNET]: '0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66',
 }
 
@@ -78,10 +85,22 @@ export const WBNB = {
   ),
 }
 
+export const WNEXI = {
+  [ChainId.NEXI]: new ERC20Token(
+    ChainId.NEXI,
+    '0x9F65526369610fD05d55fdCf4c42AFf412319D6f',
+    18,
+    'WNEXI',
+    'Wrapped NEXI',
+    'https://www.nexi.technology'
+  ),
+}
+
 export const WNATIVE: Record<number, ERC20Token> = {
   [ChainId.ETHEREUM]: WETH9[ChainId.ETHEREUM],
   [ChainId.GOERLI]: WETH9[ChainId.GOERLI],
   [ChainId.BSC]: WBNB[ChainId.BSC],
+  [ChainId.NEXI]: WNEXI[ChainId.NEXI],
   [ChainId.BSC_TESTNET]: WBNB[ChainId.BSC_TESTNET],
 }
 
@@ -103,6 +122,11 @@ export const NATIVE: Record<
   [ChainId.BSC_TESTNET]: {
     name: 'Binance Chain Native Token',
     symbol: 'tBNB',
+    decimals: 18,
+  },
+  [ChainId.NEXI]: {
+    name: 'Nexi Chain Native Token',
+    symbol: 'NEXI',
     decimals: 18,
   },
 }
